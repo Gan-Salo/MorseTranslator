a8bdd7ead409caf16b896309c09cee547d84dd71
Gan-Salo <90299377+Gan-Salo@users.noreply.github.com>
2024-09-05 13:47:39 +0700

    Update textfilehandler.cpp

--- textfilehandler.cpp
+++ textfilehandler.cpp
@@ -6,8 +6,7 @@ TextFileHandler::TextFileHandler(QObject *parent) : QObject(parent) {
 // Сохранение в файл
 void TextFileHandler::saveToFile(QString filePath, QString text) {
     QFile file(filePath);
-    if (file.open(QIODevice::WriteOnly))
-    {
+    if (file.open(QIODevice::WriteOnly)) {
         QTextStream out(&file);
         out << text;
         file.close();
@@ -18,8 +17,7 @@ void TextFileHandler::saveToFile(QString filePath, QString text) {
 QString TextFileHandler::loadFromFile(QString filePath) {
     QFile file(filePath);
     QString text;
-    if (file.open(QIODevice::ReadOnly))
-    {
+    if (file.open(QIODevice::ReadOnly)) {
         QTextStream in(&file);
         text = in.readAll();
         file.close();
